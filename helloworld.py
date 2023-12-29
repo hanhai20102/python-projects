8f714377a99ccc2af4bf7fa4fd4059a45a8575ea
sunguofeng <sgf234690@alibaba-inc.com>
2023-12-29 17:00:09 +0800

    first commit

---
+++ helloworld.py
@@ -0,0 +1,67 @@
+
+import math;
+
+print('hello world');
+
+
+
+print("c", "d", "ddd");
+
+
+
+list = [123, 123,123123];
+print(list);
+print(len(list));
+
+
+# Cache 
+
+
+
+print(list[-1]);
+
+
+
+
+list.append("asd");
+print(list);
+
+
+tumple = ('12', 23);
+
+print(tumple);
+
+
+age = 123;
+
+if age > 19:
+    print('success');
+elif age > 5:
+    print('sceond');
+else:
+    print('third');
+
+for p in list:
+    print(p); 
+
+
+map = {'Michale': 1234, '2333': "test", 123123:'oooo'}
+print(abs(-1))
+
+
+
+
+
+def my_msb(x):
+    if x > 0:
+        print("大于")
+        return map;
+    else:
+        print("小于")
+        return (1, 2);
+x, y = my_msb(18)
+
+print(x);
+
+
+
